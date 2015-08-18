e337955e7d89c7a6e2ac52807b874468c8293935
Andre Krause <akr.tester@googlemail.com>
2015-08-18 19:02:56 +0200

    -added async request to insert new task

--- src/main/java/de/saxsys/tasksapp/ui/additems/AddItemsViewModel.java
+++ src/main/java/de/saxsys/tasksapp/ui/additems/AddItemsViewModel.java
@@ -2,6 +2,7 @@ package de.saxsys.tasksapp.ui.additems;
 
 import de.saxsys.tasksapp.model.TodoItem;
 import de.saxsys.tasksapp.model.TodoItemStore;
+import javafx.application.Platform;
 import javafx.beans.binding.Bindings;
 import javafx.beans.property.BooleanProperty;
 import javafx.beans.property.ReadOnlyBooleanProperty;
@@ -13,14 +14,18 @@ import javafx.beans.property.StringProperty;
 import de.saxsys.mvvmfx.ViewModel;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpVersion;
+import org.apache.http.client.fluent.Async;
+import org.apache.http.client.fluent.Content;
 import org.apache.http.client.fluent.Request;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.concurrent.FutureCallback;
 import org.apache.http.entity.ContentType;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
+import org.json.JSONArray;
 import org.json.JSONObject;
 
 import java.io.IOException;
@@ -29,56 +34,64 @@ import java.io.IOException;
  * @author manuel.mauky
  */
 public class AddItemsViewModel implements ViewModel {
-	
-	
-	private BooleanProperty allSelected = new SimpleBooleanProperty();
-	private StringProperty newItemValue = new SimpleStringProperty("");
-	
-	private ReadOnlyBooleanWrapper allSelectedVisible = new ReadOnlyBooleanWrapper();
-	
-	public AddItemsViewModel() {
-		allSelected.addListener((obs, oldV, newV) -> {
-			TodoItemStore.getInstance().getItems()
-					.forEach(item -> item.setCompleted(newV));
-		});
-		
-		allSelectedVisible.bind(Bindings.isEmpty(TodoItemStore.getInstance().getItems()).not());
-	}
-	
-	
-	public void addItem() {
-		final String newValue = newItemValue.get();
-		if (newValue != null && !newValue.trim().isEmpty()) {
-			TodoItem newItem = new TodoItem(newValue);
-			TodoItemStore.getInstance().getItems().add(newItem);
 
-			try {
-				String result = Request.Post("http://localhost:3420/addTask")
-						.version(HttpVersion.HTTP_1_1)
-						.bodyString("{\"task\":\"" + newValue + "\"};", ContentType.APPLICATION_JSON)
-						.execute().returnContent().asString();
 
-				JSONObject json = new JSONObject(result);
-				newItem.setId(Integer.parseInt(json.get("id").toString()));
-			}
-			catch (IOException e){
-				e.printStackTrace();
-			}
+    private BooleanProperty allSelected = new SimpleBooleanProperty();
+    private StringProperty newItemValue = new SimpleStringProperty("");
 
-			newItemValue.set("");
-		}
-	}
-	
-	public StringProperty newItemValueProperty() {
-		return newItemValue;
-	}
-	
-	
-	public BooleanProperty allSelectedProperty() {
-		return allSelected;
-	}
-	
-	public ReadOnlyBooleanProperty allSelectedVisibleProperty() {
-		return allSelectedVisible;
-	}
+    private ReadOnlyBooleanWrapper allSelectedVisible = new ReadOnlyBooleanWrapper();
+
+    public AddItemsViewModel() {
+        allSelected.addListener((obs, oldV, newV) -> {
+            TodoItemStore.getInstance().getItems()
+                    .forEach(item -> item.setCompleted(newV));
+        });
+
+        allSelectedVisible.bind(Bindings.isEmpty(TodoItemStore.getInstance().getItems()).not());
+    }
+
+
+    public void addItem() {
+        final String newValue = newItemValue.get();
+        if (newValue != null && !newValue.trim().isEmpty()) {
+            //TodoItem newItem = new TodoItem(newValue);
+            //TodoItemStore.getInstance().getItems().add(newItem);
+
+            Async.newInstance().execute(Request.Post("http://localhost:3420/addTask")
+                    .version(HttpVersion.HTTP_1_1)
+                    .bodyString("{\"task\":\"" + newValue + "\"};", ContentType.APPLICATION_JSON), new FutureCallback<Content>() {
+                public void failed(final Exception ex) {
+                    System.out.println(ex.getMessage());
+                }
+
+                public void completed(final Content content) {
+                    Platform.runLater(() -> {
+                        TodoItem newItem = new TodoItem(newValue);
+                        TodoItemStore.getInstance().getItems().add(newItem);
+
+                        JSONObject json = new JSONObject(content.toString());
+                        newItem.setId(Integer.parseInt(json.get("id").toString()));
+
+                        newItemValue.set("");
+                    });
+                }
+
+                public void cancelled() {
+                }
+            });
+        }
+    }
+
+    public StringProperty newItemValueProperty() {
+        return newItemValue;
+    }
+
+
+    public BooleanProperty allSelectedProperty() {
+        return allSelected;
+    }
+
+    public ReadOnlyBooleanProperty allSelectedVisibleProperty() {
+        return allSelectedVisible;
+    }
 }
